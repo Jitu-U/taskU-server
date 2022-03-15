2ace2dcbe08472957ef006dc638d10865a86464f
Jitesh Gamit <59947307+Jitu-U@users.noreply.github.com>
2022-03-15 12:25:09 +0530

    little changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,17 +7,38 @@ routes.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
+//For initial getting all tasks
 routes.get('/tasks',(req,res) => {
   res.header("Access-Control-Allow-Origin", "*");
     Tasks.find().then( result =>  res.send(result))
     .catch( err => console.log(`Cannot get tasks ` , err));
 });
 
+
+// For creating new Task
 routes.post('/create',(req,res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.send(req.body);
    console.log(req.body);
+   const t = new Tasks();
+   t.id = req.body.id;
+   t.description = req.body.description;
+   t.deadline = req.body.deadline;
+   t.isComplete = req.body.isComplete;
+   //Pushing to Database
+    t.save()
+    .then( data => res.send(data))
+    .catch( err => console.log('ERROR',err));
 })
+
+//Delete a Task
+routes.post('/delete',(req,res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+})
+
+//Modify a Task
+routes.post('/modify'),(req,res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+}
 
 
 

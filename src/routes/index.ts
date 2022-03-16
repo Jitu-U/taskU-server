189d4e3fb8dafacd533c58eeccf098fb0c5a57aa
Jitesh Gamit <59947307+Jitu-U@users.noreply.github.com>
2022-03-16 15:40:40 +0530

    Complete Task Done

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,48 +4,89 @@ import Tasks from "../models/tasks";
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+  res.send('Hello World!');
+});
 
 //For initial getting all tasks
-routes.get('/tasks',(req,res) => {
+routes.get('/tasks', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-    Tasks.find().then( result =>  res.send(result))
-    .catch( err => console.log(`Cannot get tasks ` , err));
+  Tasks.find().then(result => res.send(result))
+    .catch(err => {
+      res.status(404).send({ error: 'Unable get tasks' });
+      console.log(`Cannot get tasks `, err)
+    });
 });
 
 
-// For creating new Task
-routes.post('/create',(req,res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-   console.log(req.body);
-   const t = new Tasks();
-   t.id = req.body.id;
-   t.description = req.body.description;
-   t.deadline = req.body.deadline;
-   t.isComplete = req.body.isComplete;
-   //Pushing to Database
-    t.save()
-    .then( data => res.send(data))
-    .catch( err => console.log('ERROR',err));
+// For creating new Taskrs
+routes.post('/create', async (req, res) => {
+  if (req.body !== undefined) {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log(req.body);
+    const t = new Tasks();
+    t.id = parseInt(req.body.id);
+    t.description = req.body.description;
+    t.deadline = req.body.deadline;
+    t.isComplete = false;
+    //Pushing to Database
+    await t.save()
+      .then(data => res.send({ data: data }))
+      .catch(err => console.log('ERROR', err));
+  } else {
+    res.send(req)
+    console.log("undefined value getting")
+  }
 })
 
-//Delete a Task
-routes.post('/delete',(req,res) => {
+//Complete a Task
+routes.post('/complete/:id', async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  const query = { id: req.body.id };
-  Tasks.findOneAndUpdate(query, { $set: { isComplete: true }});
+  const query = { id: parseInt(req.params.id) };
+  try {
+    Tasks
+      .findOneAndUpdate(query, { $set: { isComplete: true } }, { new: true }, (err, doc) => {
+        if (err) {
+          console.log(err);
+        }
+        res.send('Completed the Task')
+        console.log(doc)
+      })
+  } catch (err) {
+    res.status(404).send({ error: 'Unable to Complete' });
+  }
 })
 
 //Modify a Task
-routes.post('/modify'),(req,res) => {
+routes.post('/modify/:id'), async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const query = { id: req.params.id };
+
+  try {
+    await Tasks.findOneAndUpdate(query,
+      {
+        $set: {
+          description: req.body.description,
+          deadline: req.body.deadline
+        }
+      }
+    ).then(res.send('Successfully Edited'));
+  } catch (err) {
+    res.status(404).send({ error: 'unable to modify' });
+  }
+}
+
+//Delete a Task
+routes.post('/delete'), async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   const query = { id: req.body.id };
-  Tasks.findOneAndUpdate(query, { $set: { 
-    description: req.body.description,
-    deadline: req.body.deadline
-   }});
+
+  try {
+    await Tasks.deleteOne().then(res.send("Deletion successfull"));
+  } catch (err) {
+    res.status(404).send({ error: 'unabe to delete' });
+  }
 }
+
 
 
 
